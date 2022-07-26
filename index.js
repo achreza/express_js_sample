44ca4ee1c3f1d9adb3586b9ffe96e279df3fa069
Achmad Fahreza <81659348+achreza@users.noreply.github.com>
2022-07-26 23:42:48 +0700

    update router

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 const userRouter = require("./routes/users");
-
-app.use(bodyParser.json());
-
-app.use("/", userRouter);
-
+app.use("/users", userRouter);
 app.listen(3000, () => console.log("Server is running on port : http://localhost:3000"));
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
 const users = [
   {
@@ -12,6 +14,17 @@ const users = [
 router.get("/", (req, res) => {
   console.log(users);
   res.send(users);
+});
+
+router.post("/", jsonParser, (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send(`user dengan nama ${user.firstName} berhasil ditambahkan`);
+});
+
+router.get("/:id", (req, res) => {
+  const user = users.find((user) => user.id === req.params.id);
+  res.send(user);
 });
 
 module.exports = router;
